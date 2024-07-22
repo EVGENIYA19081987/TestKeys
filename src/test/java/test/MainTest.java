4f88fe1e4856c1810f83246a0904874acbc4eb8d
DSaraev <dvsaraev@vtb.ru>
2024-07-22 17:09:20 +0300

    refactoring

--- src/test/java/test/MainTest.java
+++ src/test/java/test/MainTest.java
@@ -4,15 +4,16 @@ import helpers.StringHelper;
 import org.testng.Assert;
 import pages.MainPage;
 
-
 import org.testng.annotations.Test;
 import pages.Users;
 public class MainTest extends BaseTest {
+
     @Test
     public void test() {
         Users users = new MainPage().clickCommunity()
                 .findTitle(StringHelper.HELP_TITLE)
                 .ClickLoop();
-        Assert.assertTrue(users.getCountOfUsers(StringHelper.HELP_TITLE));
+        Assert.assertTrue(users
+                .getCountOfUsers(StringHelper.HELP_TITLE));
     }
 }
