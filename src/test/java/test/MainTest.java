3b0cd8f0c5389153190fda4b326a6db88eaa8efb
DSaraev <dvsaraev@vtb.ru>
2024-07-22 17:15:05 +0300

    refactoring

--- src/test/java/test/MainTest.java
+++ src/test/java/test/MainTest.java
@@ -15,5 +15,9 @@ public class MainTest extends BaseTest {
                 .findTitle(StringHelper.HELP_TITLE)
                 .ClickLoop();
         Assert.assertTrue(users.getCountOfUsers(StringHelper.HELP_TITLE));
+        //todo тест ничего не проверяет. Посмотри внимательно на метод getCountOfUsers ты проверяешь,
+        // что StringHelper.HELP_TITLE содержит StringHelper.HELP_TITLE
+        // Давай упростим задачу.
+        // Получи имена всех юзеров со страницы Users и убедись, что среди них есть StringHelper.HELP_TITLE
     }
 }
