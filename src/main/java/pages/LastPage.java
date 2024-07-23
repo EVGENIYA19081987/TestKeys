f2601bd4b09299f4f5e907b619eabeb6e4486be7
EvgeniyaGlukhikh <evgeniya8787@mail.ru>
2024-07-23 12:13:33 +0300

    Community Page

--- src/main/java/pages/LastPage.java
+++ src/main/java/pages/LastPage.java
@@ -2,10 +2,10 @@ package pages;
 
 import org.openqa.selenium.By;
 
-public class LastPage extends BasePage{
-By nameUser=By.xpath("//a[text()='Black8'][1]");
+public class LastPage extends BasePage {
+    By nameUser = By.xpath("//a[text()='Black8'][1]");
 
- public boolean isDisplayed(){
-     return waitVisibilityOfElement(nameUser).isDisplayed();
- }
+    public boolean isDisplayed() {
+        return waitVisibilityOfElement(nameUser).isDisplayed();
+    }
 }
